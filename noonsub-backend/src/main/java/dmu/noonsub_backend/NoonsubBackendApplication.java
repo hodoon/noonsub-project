643c05e4edbde3e979dd65502f33de6ea9f27129
hodoon <ehgns79513@gmail.com>
2025-03-10 21:52:37 +0900

    first commit

---
+++ noonsub-backend/src/main/java/dmu/noonsub_backend/NoonsubBackendApplication.java
@@ -0,0 +1,13 @@
+package dmu.noonsub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoonsubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoonsubBackendApplication.class, args);
+	}
+
+}
